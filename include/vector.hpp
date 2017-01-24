dff8a334bb17b9b0c51a67cca75ecbf8a0c5dba3
ralismark <13449732+ralismark@users.noreply.github.com>
2017-01-24 14:35:03 +1100

    Add aliases for vectors

--- include/vector.hpp
+++ include/vector.hpp
@@ -5,3 +5,12 @@
 
 template <typename T, size_t N>
 using vector = swizzle::glsl::vector<T, N>;
+
+using vec2  = vector<double,       2>;
+using vec2i = vector<int,          2>;
+using vec2u = vector<unsigned int, 2>;
+
+using vec3  = vector<double,       3>;
+using vec3i = vector<int,          3>;
+using vec3u = vector<unsigned int, 3>;
+
