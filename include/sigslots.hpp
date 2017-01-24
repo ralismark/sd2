3d5b5617e8ee5fa64e6f10126914da1a1ae44c46
ralismark <timmy.yao@outlook.com.au>
2017-01-24 14:20:23 +1100

    Add signal/slots

---
+++ include/sigslots.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "priority_list.hpp"
+#include <functional>
+
+template <typename... Args>
+class signal
+{
+	using function_signature = void(Args...);
+	using slot_target = std::pair<std::function<function_signature>, int>;
+
+	static bool slot_less_cmp(const slot_target& lhs, const slot_target& rhs)
+	{
+		return lhs.second < rhs.second;
+	}
+
+private: // internal statics
+
+	using function_type  = std::function<function_signature>;
+	using container_type = priority_list<slot_target, decltype(slot_less_cmp)>;
+	using slot_id        = typename container_type::const_iterator;
+
+private: // variables
+
+	container_type slotlist;
+
+public: // methods
+
+	signal()
+		: slotlist(&slot_less_cmp)
+	{
+	}
+
+	slot_id connect(function_type fn, int priority = 0)
+	{
+		return slotlist.emplace(std::move(fn), priority);
+	}
+
+	void disconnect(slot_id slot)
+	{
+		slotlist.erase(slot);
+	}
+
+	void disconnect_all()
+	{
+		slotlist.clear();
+	}
+
+	void emit(Args... args)
+	{
+		for(auto& slot : slotlist) {
+			slot.first(args...);
+		}
+	}
+
+	void operator()(Args... args)
+	{
+		this->emit(std::forward<Args>(args)...);
+	}
+
+	slot_id operator+=(function_type fn)
+	{
+		return this->connect(std::move(fn));
+	}
+
+};
