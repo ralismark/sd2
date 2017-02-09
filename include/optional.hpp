07039edd70d7bb3fca3653a60d75039d1360d399
ralismark <timmy.yao@outlook.com.au>
2017-02-09 20:54:07 +1100

    Fix headers

--- include/optional.hpp
+++ include/optional.hpp
@@ -1,7 +1,33 @@
 #pragma once
 
-#include <stx/optional>
+#include "config.hpp"
+
+#if defined(HAS_OPTIONAL)
+
+#include <optional>
 
 namespace stx {
-	using namespace std::experimental; // works even with multiple defs
+	using std::optional;
+	using std::bad_optional_access;
+	using std::nullopt_t;
+	constexpr auto& nullopt = std::nullopt;
+	using std::make_optional;
 }
+
+#elif defined(HAS_EXP_OPTIONAL)
+
+#include <experimental/optional>
+
+namespace stx {
+	using std::experimental::optional;
+	using std::experimental::bad_optional_access;
+	using std::experimental::nullopt_t;
+	constexpr auto& nullopt = std::experimental::nullopt;
+	using std::experimental::make_optional;
+}
+
+#else
+
+#error "no <optional> or <experimental/optional> available"
+
+#endif
