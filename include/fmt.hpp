ee52c27ca268a14d0d5a270e78043d1f24bbcb5a
ralismark <timmy.yao@outlook.com.au>
2017-02-12 23:11:07 +1100

    Suppress warnings from external libraries

--- include/fmt.hpp
+++ include/fmt.hpp
@@ -1,5 +1,15 @@
 #pragma once
 
+#include "core/preproc.hpp"
+
+_pp_warn_push();
+
+#if defined(COMPILER_CLANG)
+	_pp_warn_stop("-Wexpansion-to-defined");
+#endif
+
 #define FMT_HEADER_ONLY
 #include "depends/fmt/fmt/format.h"
 #include "depends/fmt/fmt/ostream.h"
+
+_pp_warn_pop();
