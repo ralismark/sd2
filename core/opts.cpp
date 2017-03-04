eb6fdd50b74dc0f03dec5853cf032ba00049684c
ralismark <13449732+ralismark@users.noreply.github.com>
2017-03-04 14:46:11 +1100

    Clean up core/opts

--- core/opts.cpp
+++ core/opts.cpp
@@ -1,5 +1,6 @@
 #include "opts.hpp"
 
+#include <cassert>
 #include <getopt.h>
 #include <sfml/window/windowstyle.hpp>
 
@@ -17,8 +18,8 @@ namespace rt {
 		int wstyle = sf::Style::Titlebar | sf::Style::Close;
 		vec2i wsize{1024, 600};
 
-		opt_result parse(int argc, char** argv)
-		{
+		namespace { // anonymous
+
 			const char* help =
 R"({0} - standard window
 
@@ -38,23 +39,78 @@ program using this runtime, or may be ignored.
 )";
 
 			const char* err = "{}: {}: {}\n";
-			auto parse_int = [pgname = argv[0], err] (const std::string& s, const std::string& disp_s) -> stx::optional<int>
-				{
-					try {
-						size_t idx = 0;
-						int out = std::stoi(s, &idx);
-						if(idx < s.size() || out < 1) {
-							throw std::invalid_argument("");
-						}
-						return out;
-					} catch(const std::invalid_argument&) {
-						fmt::print(err, pgname, disp_s, "invalid format");
-					} catch(const std::out_of_range&) {
-						fmt::print(err, pgname, disp_s, "too large to be representable");
-					}
-					return stx::nullopt;
-				};
 
+			stx::optional<int> parse_int(const char* pgname, const std::string& s, const std::string& disp_s)
+			{
+				try {
+					size_t idx = 0;
+					int out = std::stoi(s, &idx);
+					if(idx < s.size() || out < 1) {
+						throw std::invalid_argument("");
+					}
+					return out;
+				} catch(const std::invalid_argument&) {
+					fmt::print(err, pgname, disp_s, "invalid format");
+				} catch(const std::out_of_range&) {
+					fmt::print(err, pgname, disp_s, "too large to be representable");
+				}
+				// if exception is thrown
+				return stx::nullopt;
+			}
+
+			bool parse_set_winstyle(const char* pgname, const std::string& arg)
+			{
+				wstyle = 0;
+				for(auto& c : arg) {
+					switch(c) {
+					case 't':
+						wstyle |= sf::Style::Titlebar;
+						break;
+					case 'r':
+						wstyle |= sf::Style::Resize;
+						break;
+					case 'c':
+						wstyle |= sf::Style::Close;
+						break;
+					case 'f':
+						wstyle |= sf::Style::Fullscreen;
+						break;
+					default:
+						fmt::print(err, pgname, arg, "invalid character flag");
+						return false;
+					}
+				}
+				if((wstyle & sf::Style::Fullscreen) && (wstyle != sf::Style::Fullscreen)) {
+					fmt::print(err, pgname, arg, "fullscreen cannot be combined with other flags");
+					return false;
+				}
+				return true;
+			}
+
+			bool parse_set_winsize(const char* pgname, const std::string& arg)
+			{
+				auto split = arg.find_first_of(',');
+				if(split == std::string::npos) {
+					fmt::print(err, argv[0], arg, "invalid format");
+					return false;
+				}
+				auto x = parse_int(argv[0], arg.substr(0, split), arg);
+				if(!x) {
+					return false;
+				}
+				auto y = parse_int(argv[0], arg.substr(split + 1), arg);
+				if(!y) {
+					return false;
+				}
+
+				wsize.x = *x;
+				wsize.y = *y;
+			}
+
+		} // namespace anonymous
+
+		opt_result parse(int argc, char** argv)
+		{
 			static option long_opts[] = {
 				{"fps",   optional_argument, 0, 'f'},
 				{"style", optional_argument, 0, 'w'},
@@ -62,27 +118,29 @@ program using this runtime, or may be ignored.
 				{"help",  no_argument,       0, 'h'},
 			};
 
+			const char* short_opts = "hf::w::s:a::b::c::";
+
 			int opt_index;
 			int opt;
-			while((opt = getopt_long(argc, argv, "hf::w::s:a::b::c::", long_opts, &opt_index)) != -1) {
-				std::string arg = optarg ? optarg : "";
+			while((opt = getopt_long(argc, argv, short_opts, long_opts, &opt_index)) != -1) {
+				std::string arg = optarg ? optarg : ""; // for convenience
 
 				switch(opt) {
 				case 'a':
-					a = optarg ? optarg : "";
+					a = arg;
 					break;
 				case 'b':
-					b = optarg ? optarg : "";
+					b = arg;
 					break;
 				case 'c':
-					c = optarg ? optarg : "";
+					c = arg;
 					break;
 				case 'h':
 					fmt::print(help, argv[0]);
 					return parse_help;
 				case 'f':
 					if(optarg) {
-						wfps = parse_int(arg, arg);
+						wfps = parse_int(argv[0], arg, arg);
 						if(!wfps) {
 							return parse_fail;
 						}
@@ -91,58 +149,20 @@ program using this runtime, or may be ignored.
 					}
 					break;
 				case 'w':
-					wstyle = 0;
-					if(optarg) {
-						for(auto& c : arg) {
-							switch(c) {
-							case 't':
-								wstyle |= sf::Style::Titlebar;
-								break;
-							case 'r':
-								wstyle |= sf::Style::Resize;
-								break;
-							case 'c':
-								wstyle |= sf::Style::Close;
-								break;
-							case 'f':
-								wstyle |= sf::Style::Fullscreen;
-								break;
-							default:
-								fmt::print(err, argv[0], arg, "invalid character flag");
-								return parse_fail;
-							}
-						}
-						if((wstyle & sf::Style::Fullscreen) && (wstyle != sf::Style::Fullscreen)) {
-							fmt::print(err, argv[0], arg, "fullscreen cannot be combined with other flags");
-							return parse_fail;
-						}
+					if(!parse_set_winstyle(argv[0], arg)) {
+						return parse_fail;
 					}
 					break;
 				case 's':
-					{
-						auto split = arg.find_first_of(',');
-						if(split == std::string::npos) {
-							fmt::print(err, argv[0], arg, "invalid format");
-							return parse_fail;
-						}
-						auto x = parse_int(arg.substr(0, split), arg);
-						if(!x) {
-							return parse_fail;
-						}
-						auto y = parse_int(arg.substr(split + 1), arg);
-						if(!y) {
-							return parse_fail;
-						}
-
-						wsize.x = *x;
-						wsize.y = *y;
+					if(!parse_set_winsize(argv[0], arg)) {
+						return parse_fail;
 					}
 					break;
 				case '?':
 				case ':':
 					return parse_fail;
 				default:
-					throw std::logic_error("unhandled option");
+					assert(false && "unhandled option");
 				}
 			}
 			return parse_success;
