a7ed60b7de8a6f6021314a8859ee6153a0c3e9f3
ralismark <timmy.yao@outlook.com.au>
2017-07-29 01:41:18 +1000

    Document various core files

--- core/math_constants.hpp
+++ core/math_constants.hpp
@@ -1,9 +1,24 @@
 #pragma once
 
+/**
+ * \file
+ * \brief Mathematical constants
+ *
+ * This file provides common mathematical constants. These are calculated using
+ * math functions (e.g. acos(-1) for pi), not stored constants. However, they
+ * are only calculated once. All functions for these are stored in namespace
+ * constant.
+ */
+
 namespace constant {
 
+	// Pi, ratio of diameter to circumference
 	long double pi();
+
+	// E, natural constant
 	long double e();
+
+	// Square root of 2, the number that is 2 when squared.
 	long double sqrt2();
 
 } // namespace constant
