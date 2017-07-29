86be1e12e23b7eba857a57c01eb76d490e283973
ralismark <timmy.yao@outlook.com.au>
2017-07-29 15:39:51 +1000

    Update documentation for disp

--- disp/window.hpp
+++ disp/window.hpp
@@ -22,19 +22,30 @@
  * a way to access and control it. Generally, it acts as a `pointer', using the
  * dereference operators to access the underlying singleton.
  *
- * The configuration of the window is done using macros. See config.hpp for
- * defaults, as well as the format of certain options.
+ * If using the provided main(), the configuration can be modified using
+ * command line arguments.
+ *
+ * \warning
+ * The init() method must be called before using the window. If you're using
+ * the provided main() in the runtime, this is already done for you.
  */
 class stdwindow
 {
 public: // statics
 
+	/// Underlying singleton window type
 	using window_type = sf::RenderWindow;
 
-	// confix
+	/// Window name
 	static constexpr auto winname = "Standard window";
+
+	/// Window style, using SFML window styles
 	static int winstyle;
+
+	/// FPS Limit. If negative or 0, no limit is set
 	static int winfps;
+
+	/// Window drawable area size
 	static vec2i winsize;
 
 private: // internal statics
@@ -51,8 +62,10 @@ public: // methods
 
 	// helpers
 
+	/// Initialise the window, using the static variables. Do not initialise more than once
 	void init();
 
+	/// Find if the window has been initialised
 	explicit operator bool() const;
 	bool operator!() const;
 
@@ -68,4 +81,12 @@ public: // methods
 
 };
 
+/**
+ * \var stdwin
+ * \brief Instance of stdwindow
+ *
+ * This provides a unified access point to the underlying window singleton in
+ * stdwindow. Use this if you want, since all instances point to the same
+ * window.
+ */
 extern stdwindow stdwin;
